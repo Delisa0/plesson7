80e9b725c3e34462647030d34d950a9613f6e02e
Delisa0 <elahidelisa0@gmail.com>
2025-07-29 17:58:22 +1000

    dictionary

---
+++ activity 2.py
@@ -0,0 +1,9 @@
+my_dict = {1:'hello', 2:'how are you', 3:'bye', 4:[4,2,3,6]}
+
+print(my_dict[1], my_dict[2])
+my_dict[3] = 'namaste'
+print(my_dict)
+my_dict[5] = 'Konnichiwa'
+print(my_dict)
+my_dict.clear()
+print(my_dict)
